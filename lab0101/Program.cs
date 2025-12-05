aeaaf852ea70f33cb7c9102ae33927fdce18c379
Sant1agoAm1gos <kn1b24.skyrdenko@kpnu.edu.ua>
2025-12-05 13:01:38 +0200

    0101

---
+++ lab0101/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Конвертація валют");
+
+        Console.Write("введіть суму у гривнях: ");
+        double uah = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("введіть курс долара: ");
+        double rate = Convert.ToDouble(Console.ReadLine());
+
+        double usd = uah / rate;
+        Console.WriteLine("в доларах: " + Math.Round(usd, 2));
+    }
+}
